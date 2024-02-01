030cae4bb6db440d773057ae5027826d635c5927
MepilLeaf <77562979+MepilLeaf@users.noreply.github.com>
2024-02-01 12:41:34 -0800

    Created cards for the landing page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CreateRecipe from "./pages/CreateRecipes/CreateRecipe";
 import BrowseRecipes from "./pages/BrowseRecipes/BrowseRecipes";
+import Landing from "./pages/Landing/Landing";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Landing />} />
           <Route path="/create" element={<CreateRecipe />} />
           <Route path="/browse" element={<BrowseRecipes />} />
           <Route path="/browse/:id" element={<BrowseRecipes />} />
